290e37224ebc2bf5f6e8f7fcda30be66dd3221f1
Joe Lafiosca <joe@hemlane.com>
2022-02-12 14:04:58 -0500

    demonstrate router inconsistency

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  rewrites: async () => ({
+    fallback: [
+      {
+        source: '/:path*',
+        destination: `http://localhost:4444/:path*`,
+      },
+    ],
+  }),
 }
 
 module.exports = nextConfig
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,26 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
+import { useRouter } from 'next/router';
+import { useCallback, useEffect } from 'react';
+
+const useRouteChangeHandler = (): void => {
+  const router = useRouter();
+  const onRouteChangeComplete = useCallback((url: string) => {
+    console.log('route change', url);
+  }, []);
+  useEffect(() => {
+    console.log('route change handler enabled');
+    router.events.on('routeChangeComplete', onRouteChangeComplete);
+    return () => {
+      console.log('route change handler disabled');
+      router.events.off('routeChangeComplete', onRouteChangeComplete);
+    };
+  }, [router.events, onRouteChangeComplete]);
+};
+
+const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
+  useRouteChangeHandler();
   return <Component {...pageProps} />
 }
 
